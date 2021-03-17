926ca3003b90eb5fbd785f1a568376d85e6564a4
Federica <federica.manograssi@gmail.com>
2021-03-17 16:30:38 +0100

    MILESTONE 01 js ok

---
+++ js/main.js
@@ -0,0 +1,132 @@
+$(document).ready(function () {
+const list = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+// Milestone 1
+// Partendo dalla struttura dati che troviamo sotto, mostriamo in pagina tutte le icone disponibili come da layout.
+
+//01.1 identifichiamo dove andranno inserite le icone
+const container = $('.icons_container');
+
+// 01.2 andiamo a creare le nostre icone coun un ciclo for all'interno dell'array(vedi giù per la funzione)
+
+print(list,container);
+
+});
+
+
+// ____________________
+//FUNCTIONS
+
+// 01.2 FUNCTION PER STAMPARE ICONE
+function print(array,container){
+	array.forEach((icon) =>{
+		const {name,prefix,family} = icon;
+	
+		const iconHTML =`
+		<div>
+			<i class="${family} ${prefix}${name}"></i>
+			<div>${name}</div>
+		</div>
+		`;
+	
+		container.append(iconHTML);
+	})
+
+}
